906133639b75f86097b0ab64c5d5f274fb93eebe
te28teght-glitch <te28teght@gmail.com>
2026-06-02 21:32:53 +0300

    ref: button + pole

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,6 +10,42 @@ namespace SyncCurrencyConverter
         public MainPage()
         {
             InitializeComponent();
+            
+            // Подписываем событие для поля ввода
+            amountEntry.TextChanged += OnEntryTextChanged;
+        }
+
+        // Событие при изменении текста в поле ввода
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string inputValue = e.NewTextValue; // Получаем новый текст
+            string inputSum = e.NewTextValue;
+            if (string.IsNullOrEmpty(inputValue))
+            {
+                statusLabel.Text = "Введите сумму в рублях...";
+                return;
+            }
+
+            if ()
+            {
+                
+            }
+            
+            if (double.TryParse(inputValue, out double amount))
+            {
+                if (selectedButton != null)
+                {
+                    statusLabel.Text = $"Выбрана: {selectedButton.Text}, Сумма: {amount} ₽";
+                }
+                else
+                {
+                    statusLabel.Text = $"Сумма: {amount} ₽. Выберите валюту";
+                }
+            }
+            else
+            {
+                statusLabel.Text = "Введите корректное число!";
+            }
         }
 
         private void OnButtonClicked(object sender, EventArgs e)
@@ -29,7 +65,8 @@ namespace SyncCurrencyConverter
             clickedButton.TextColor = Colors.White;
             
             selectedButton = clickedButton;
-
+            
+    
         }
     }
 }
